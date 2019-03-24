48dbef688fdd775c1f4abb829dae1d4b07907525
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-03-24 09:31:12 -0400

    Add basic scrape route for gathering new articles

--- controllers/scrape_controller.js
+++ controllers/scrape_controller.js
@@ -8,7 +8,32 @@ const cheerio = require("cheerio");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index", req);
+  res.render("index", req);
 });
+
+router.get("/scrape", (req, res) => {
+  axios.get("https://www.theguardian.com/football").then(response => {
+    const $ = cheerio.load(response.data);
+
+    let result = {};
+    $(".fc-item__content").each(function(i, element) {
+      result.title = $(element)
+        .find(".fc-item__title")
+        .text();
+      result.link = $(element)
+        .find("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(dbArticle => {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(err => console.log(err));
+        });
+
+    res.send(`Scrape complete!`);
+    });
+  });
 
 module.exports = router;
