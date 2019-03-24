8225fccc61020507989cf1ff63298b15fca14b7a
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-03-24 19:14:39 -0400

    Add basic handling for the save modal.

--- public/js/saved.js
+++ public/js/saved.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+  const thisArticle = {
+    id: null,
+    setId: function(id) {
+      this.id = id;
+    },
+    getId: function() {
+      return this.id;
+    }
+  };
+
   $(".delete-article").on("click", deleteArticle);
-  $(".save-article").on("click", showSaveModal);
+  $(".add-note").on("click", showAddNoteModal);
+  $("#submit-note").on("click", submitNote);
 
   // Delete an article from the database and the DOM when clicked.
   function deleteArticle() {
@@ -9,12 +20,42 @@ $(document).ready(function() {
     thisCard.remove();
 
     $.ajax({
-        type: "DELETE",
-        url: `/api/article/${id}`,
+      type: "DELETE",
+      url: `/api/article/${id}`
     }).then(data => console.log(data));
   }
 
-  function showSaveModal() {
+  //   Show the modal when click on the save article button.
+  function showAddNoteModal(e) {
     $(".modal").modal("show");
+    thisArticle.setId($(this).data("id"));
+  }
+
+  function submitNote(e) {
+    e.preventDefault();
+
+    const title = $("#note-title")
+      .val()
+      .trim();
+    const body = $("#note-text")
+      .val()
+      .trim();
+    const id = thisArticle.getId();
+
+    const data = {
+      articleId: id,
+      title: title,
+      body: body
+    };
+
+    // Send our data to the notes POST route.
+    $.post("/api/notes/", data, (data, status) =>
+      console.log(`Status: ${status}. Data: ${data}`)
+    );
+
+    // Clear the note modal inputs.
+    $("#note-title").val("");
+    $("#note-text").val("");
+    $(".modal").modal("hide");
   }
 });
